507744ff792b127e4e66a03011cee66b378812b6
rouzbehsbz <rouzbehsbz@gmail.com>
2022-10-28 17:48:20 +0330

    add auto crawler robot

--- src/scripts/autoCrawlerScript.ts
+++ src/scripts/autoCrawlerScript.ts
@@ -1,5 +1,24 @@
+import axios from "axios";
+
+const url = "http://164.90.213.118:8080/api/v1";
+
 async function main() {
   try {
+    const findCountries = await axios.get(`${url}/countries`);
+    const findMinerals = await axios.get(`${url}/minerals`);
+
+    for (const country of findCountries.data.result) {
+      for (const mineral of findMinerals.data.result) {
+        await axios.get(
+          `${url}/processes/production-rates?country=${country}&mineral=${mineral}`
+        );
+
+        console.log(
+          `Getting data of ${country} for ${mineral} has been finished.`
+        );
+      }
+    }
+
     process.exit(0);
   } catch (err) {
     console.log(err);
